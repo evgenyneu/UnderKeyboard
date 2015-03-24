f79880ad61d0ed9cd4dfa10d85e39e2a33dab377
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2015-03-24 16:28:13 +1100

    Initial Commit

---
+++ UnderKeyboardDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UnderKeyboardDemo
+//
+//  Created by Evgenii Neumerzhitckii on 24/03/2015.
+//  Copyright (c) 2015 The Exchange Group Pty Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
