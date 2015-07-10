9242d002e6d268d4868c8d26b2d3ebd25b09407e
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2015-07-10 11:09:07 +1000

    Using keyboard observer

--- UnderKeyboard/UnderKeyboard.swift
+++ UnderKeyboard/UnderKeyboard.swift
@@ -13,6 +13,8 @@ public class UnderKeyboard {
   private weak var scrollView: UIScrollView?
   private weak var bottomLayoutGuide: UILayoutSupport?
   
+  private let keyboardObserver = UnderKeyboardObserver()
+  
   private class var shared: UnderKeyboard {
     return tegUnderKeyboard
   }
@@ -30,31 +32,54 @@ public class UnderKeyboard {
   }
   
   private init() {
-    registerForKeyboardNotifications()
+    keyboardObserver.start()
+//    registerForKeyboardNotifications()
+    
+    keyboardObserver.willAnimate = keyboardWillShow
+      
   }
   
   deinit {
-    unRegisterForKeyboardNotifications()
+//    unRegisterForKeyboardNotifications()
   }
+//  
+//  private func registerForKeyboardNotifications() {
+//    NSNotificationCenter.defaultCenter().addObserver(self,
+//      selector: "keyboardDidShow:",
+//      name: UIKeyboardDidShowNotification, object: nil)
+//    
+//    NSNotificationCenter.defaultCenter().addObserver(self,
+//      selector: "keyboardWillHide:",
+//      name: UIKeyboardWillHideNotification, object: nil)
+//  }
+//  
+//  private func unRegisterForKeyboardNotifications() {
+//    NSNotificationCenter.defaultCenter().removeObserver(self)
+//  }
   
-  private func registerForKeyboardNotifications() {
-    NSNotificationCenter.defaultCenter().addObserver(self,
-      selector: "keyboardDidShow:",
-      name: UIKeyboardDidShowNotification, object: nil)
-    
-    NSNotificationCenter.defaultCenter().addObserver(self,
-      selector: "keyboardWillHide:",
-      name: UIKeyboardWillHideNotification, object: nil)
-  }
-  
-  private func unRegisterForKeyboardNotifications() {
-    NSNotificationCenter.defaultCenter().removeObserver(self)
+  func keyboardWillShow(isShowing: Bool, beginHeight: CGFloat, endHeight: CGFloat) {
+    if isShowing {
+      let layoutGuideHeight = bottomLayoutGuide?.length ?? 0
+      let height = endHeight - layoutGuideHeight
+
+      let insets =  UIEdgeInsets(
+        top: 0,
+        left: 0,
+        bottom: height,
+        right: 0)
+
+      scrollView?.contentInset = insets
+      scrollView?.scrollIndicatorInsets = insets
+    } else {
+      scrollView?.contentInset = UIEdgeInsetsZero;
+      scrollView?.scrollIndicatorInsets = UIEdgeInsetsZero;
+    }
   }
   
   func keyboardDidShow(notification: NSNotification) {
     if let userInfo = notification.userInfo,
-      let value = userInfo[UIKeyboardFrameBeginUserInfoKey] as? NSValue {
-        
+      let value = userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue {
+
         let layoutGuideHeight = bottomLayoutGuide?.length ?? 0
         let height = value.CGRectValue().size.height - layoutGuideHeight
         
---
+++ UnderKeyboard/UnderKeyboardObserver.swift
@@ -0,0 +1,56 @@
+public final class UnderKeyboardObserver: NSObject {
+  public typealias AnimationCallback = (isShowing: Bool, beginHeight: CGFloat, endHeight: CGFloat) -> ()
+  public typealias AnimationEnd = (isShowing: Bool, finished: Bool, beginHeight: CGFloat, endHeight: CGFloat) -> ()
+  
+  let notificationCenter: NSNotificationCenter
+  
+  public var willAnimate: AnimationCallback?
+  public var animate: AnimationCallback?
+  public var didAnimate: AnimationEnd?
+  
+  public override init() {
+    notificationCenter = NSNotificationCenter.defaultCenter()
+    super.init()
+  }
+  
+  deinit {
+    notificationCenter.removeObserver(self)
+  }
+  
+  public func start() {
+    stop()
+    
+    notificationCenter.addObserver(self, selector: Selector("keyboardNotification:"), name:UIKeyboardWillShowNotification, object: nil);
+    notificationCenter.addObserver(self, selector: Selector("keyboardNotification:"), name:UIKeyboardWillHideNotification, object: nil);
+  }
+  
+  public func stop() {
+    notificationCenter.removeObserver(self)
+  }
+  
+  // MARK: - Notification
+  
+  func keyboardNotification(notification: NSNotification) {
+    let isShowing = notification.name == UIKeyboardWillShowNotification
+    
+    if let userInfo = notification.userInfo,
+      let beginHeigth = (userInfo[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue().height,
+      let endHeight = (userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.CGRectValue().height,
+      let duration: NSTimeInterval = (userInfo[UIKeyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue,
+      let animationCurveRawNSN = userInfo[UIKeyboardAnimationCurveUserInfoKey] as? NSNumber {
+          
+      willAnimate?(isShowing: isShowing, beginHeight: beginHeigth, endHeight: endHeight)
+        
+      UIView.animateWithDuration(duration,
+        delay: NSTimeInterval(0),
+        options: UIViewAnimationOptions(rawValue: animationCurveRawNSN.unsignedLongValue),
+        animations: { [weak self] in
+          self?.animate?(isShowing: isShowing, beginHeight: beginHeigth, endHeight: endHeight)
+        },
+        completion: { [weak self] finished in
+          self?.didAnimate?(isShowing: isShowing, finished: finished, beginHeight: beginHeigth, endHeight: endHeight)
+        }
+      )
+    }
+  }
+}
