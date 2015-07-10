a42685cd57469b4c2bccab6ed3a047363970cf1a
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2015-07-10 13:22:01 +1000

    Add icons

--- Demo/LoginViewController.swift
+++ Demo/LoginViewController.swift
@@ -3,8 +3,17 @@ import UIKit
 import UnderKeyboard
 
 class LoginViewController: UIViewController {
+  @IBOutlet weak var bottomLayoutConstraint: NSLayoutConstraint!
+  var initialBottomConstraintConstant: CGFloat = 0
+  var underKeyboardObserver = UnderKeyboardObserver()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    underKeyboardObserver.start()
+    
+    initialBottomConstraintConstant = bottomLayoutConstraint.constant
+    underKeyboardObserver.willAnimate = keyboardWillAnimate
+    underKeyboardObserver.animate = keyboardAnimate
   }
   
   @IBAction func didTapCloseKeyboard(sender: AnyObject) {
@@ -13,5 +22,19 @@ class LoginViewController: UIViewController {
   
   @IBAction func didTapLoginButton(sender: AnyObject) {
     view.endEditing(false)
+  }
+  
+  func keyboardWillAnimate(isShowing: Bool, height: CGFloat) {
+    let correctedHeight = height - bottomLayoutGuide.length
+    
+    if isShowing {
+      bottomLayoutConstraint.constant = correctedHeight + initialBottomConstraintConstant
+    } else {
+      bottomLayoutConstraint.constant = initialBottomConstraintConstant
+    }
+  }
+  
+  func keyboardAnimate(isShowing: Bool, height: CGFloat) {
+    view.layoutIfNeeded()
   }
 }
