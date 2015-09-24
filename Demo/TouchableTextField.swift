961512dc4651f1d28994c0e34c78da4a8ca13e7b
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2015-09-25 08:53:25 +1000

    Make input touch target bigger

---
+++ Demo/TouchableTextField.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+
+/*
+
+A text field with enlarged touch target for easier selection.
+
+*/
+class TouchableTextField: UITextField {
+  /// Increase the hitsize of the image view if it's less than 44px for easier tapping.
+  override func pointInside(point: CGPoint, withEvent event: UIEvent?) -> Bool {
+    let oprimizedBounds = LargeTouchTarget.optimize(bounds)
+    return oprimizedBounds.contains(point)
+  }
+}
