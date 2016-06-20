e352534b161c08526099b07c141fa0a515397ba5
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2016-06-20 17:36:40 +1000

    Update to Swift 3.0

--- Demo/AppDelegate.swift
+++ Demo/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     UITabBar.appearance().tintColor = UIColor.white()
     
+    UIApplication.shared().statusBarStyle = .lightContent
     // Override point for customization after application launch.
     return true
   }
